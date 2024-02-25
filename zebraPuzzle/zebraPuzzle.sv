08505f3ac5782af62bfb46c242d3016a5f050596
Akshay Rai <rai.axay@gmail.com>
2024-02-25 15:58:24 +0530

    add zebra puzzle

---
+++ zebraPuzzle/zebraPuzzle.sv
@@ -0,0 +1,9 @@
+typedef enum {RED, GREEN, IVORY, YELLOW, BLUE ,} color_e ;
+
+typedef enum {ENGLISH, SPANISH, UKRAINE, NORWAY, JAPANESE} country_e ;
+
+typedef enum {DOG, SNAILS, FOX, HORSE, ZEBRA } pet_e;
+
+typedef enum {COFFEE, TEA, MILK, ORANGE_JUICE, WATER } drink_e ;
+
+typedef enum {OLD_GOLD, KOOL, CHESTERFIELD, LUCKY_STRIKE, PARLIAMENT}  smoke_e;
